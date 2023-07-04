532b80789eafb4ce0aae6021d9d36afa8d7abaa3
Haarik05 <haarik05@gmail.com>
2023-07-04 20:20:38 +0530

    added validation_main file

---
+++ apps/product-catalogue/src/app/routes/validation_main.ts
@@ -0,0 +1,100 @@
+export const mandatoryParamCheck = (data)=>{
+
+    const missingparams = []
+    const mandatoryParam = [
+        'merchantId',
+        'productCatalogueId',
+    
+        'price',
+        'tax',
+    ]
+    mandatoryParam.forEach((val)=>{
+        const incomingKeys = Object.keys(data)
+        if(!incomingKeys.includes(val)){
+            missingparams.push(val)
+        }
+    })
+    if (missingparams.length){
+        throw new Error (`Mandatory params missing. ${missingparams.join(',')}`)
+    }
+
+}
+export const merchantId = (id)=>{
+    if(typeof id!=='string')
+        throw new Error(`Invalid merchantId type`)
+}
+export const productCatalogueId = (productCatalogueId)=>{
+    if(typeof productCatalogueId!=='string')
+        throw new Error (`Invalid productCatalogueId type`)
+}
+
+export const tax = (tax)=>{
+    if(typeof tax!=='number')
+        throw new Error(`Invalid tax type`)
+    if(tax < 0)
+        throw new Error(`Tax should be positive value`)
+}
+
+export const price = (price)=>{
+    if(typeof price !=='number')
+        throw new Error(`Invalid price type`)
+    if(price < 0)
+        throw new Error(`rice should be  positive value.`)
+}
+
+export const description = (description)=>{
+    if(typeof description!=='string')
+        throw new Error(`Invalid description type`)
+    if(description.length > 200)
+        throw new Error(`Description length should be less than 200 characters.`)
+}
+
+export const brand = (brand)=>{
+    if(typeof brand !=='string')
+        throw new Error(`Invalid brand type`)
+    if(brand.length <3 || brand.length >30)
+        throw new Error(`Brand length should be between 3 and 30 characters.`)
+}
+
+export const discount  = (discount)=>{
+    if(typeof discount !=='number')
+        throw new Error(`Invalid discount type`)
+}
+
+export const name = (name)=>{
+    if(typeof name!=='string')
+        throw new Error(`Invalid type name`)
+    if(name.length<3 || name.length > 30)
+        throw new Error(`Name length should be between 3 and 30 characters.`)
+}
+
+export const typeValidationCheck = (data)=>{
+        if( typeof data.merchantId!=='string' ||
+            typeof data.productCatalogueId!=='string' ||
+            typeof data.tax !== 'number' ||
+            typeof data.price !=='number' 
+        ){
+            throw new Error (`Invalid type`)
+        }
+        
+        if (data.description && typeof data.description !== 'string')
+            throw new Error(`Invalid description type.`);
+        if (data.brand && typeof data.brand !== 'string')
+            throw new Error(`Invalid brand type.`);
+        if (data.discount && typeof data.discount !== 'number')
+                throw new Error(`Invalid discount type.`);
+}
+
+const validation ={
+    mandatoryParamCheck,
+    typeValidationCheck,
+    merchantId,
+    productCatalogueId,
+    tax,
+    price,
+    description,
+    brand,
+    discount,
+    name,
+};
+export default validation
